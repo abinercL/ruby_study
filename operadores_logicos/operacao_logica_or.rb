d2dcca59585ce67583095d2d67e2a6991ab2cab5
Abinercl <abiner17costa@gmail.com>
2024-06-03 14:14:04 -0300

    Operação logica or utilizando ||

---
+++ operadores_logicos/operacao_logica_or.rb
@@ -0,0 +1,7 @@
+#A operação logica or pode ser executada utilizando um dos dois operadores logicos "or" ou "||"
+#exemplo de codigo
+
+puts "true or true = #{true or true}"
+puts "true || false = #{true || false}"
+puts "false or true = #{false or true}"
+puts "false || true = #{false || true}"
