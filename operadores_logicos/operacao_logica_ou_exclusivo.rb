6428547357c58448385316c86db27a91e5bb859c
Abinercl <abiner17costa@gmail.com>
2024-06-03 14:23:16 -0300

    Operação logica ou exclusivo ^

---
+++ operadores_logicos/operacao_logica_ou_exclusivo.rb
@@ -0,0 +1,10 @@
+#A operação lógica ‘ou exclusivo’, também conhecida como xor (do inglês exclusive or)
+#retorna verdadeiro quando apenas um dos operadores é verdadeiro.
+#Em todos os outros casos, será retornado falso.
+
+#exemplo codigo
+
+puts "true ^ true = #{true ^ true}"
+puts "true ^ false = #{true ^ false}"
+puts "false ^ true = #{false ^ true}"
+puts "false ^ true = #{false ^ true}"
