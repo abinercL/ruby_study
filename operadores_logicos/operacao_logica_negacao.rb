6bf7813d6211885864c3bd650e9a631521b8c4e5
Abinercl <abiner17costa@gmail.com>
2024-06-03 14:29:47 -0300

    Operação logica negação utilizando not ou !

---
+++ operadores_logicos/operacao_logica_negacao.rb
@@ -0,0 +1,7 @@
+# A operação logica de nagação inverte o valor logicco do operando. se o operando for verdadeiro ele se tornara falso
+#e se for falso se tornara verdadeiro.
+
+#A operação logica "not" pode ser executada utilizando um dos seguintes operadores: "not" ou "!"
+
+puts "not true = #{not true}"
+puts "! false = #{!false}"
