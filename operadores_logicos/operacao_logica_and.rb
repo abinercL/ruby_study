f2f7344386f5bd2b2c51ba3dde8fc9a917793209
Abinercl <abiner17costa@gmail.com>
2024-06-03 13:46:54 -0300

    Operação logica and utilizando &&

---
+++ operadores_logicos/operacao_logica_and.rb
@@ -0,0 +1,9 @@
+#A operação logica "and" pode ser executada utilizando um dos dois operadores logicos:
+#"and" ou "&&"
+
+#exemplo 1 de codigo
+
+puts "trues and true = #{true and true}"
+puts "true && false = #{true && false}"
+puts "false and true = #{false and true}"
+puts "false && false = #{false and false}"
