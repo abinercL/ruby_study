531e17e8859e21bf36fb12477b0e8bcbb5c2ab9f
abinercL <abiner17costa@gmail.com>
2024-07-16 14:38:29 -0300

    Atividade 8.1 feita de maneira direta sem usar nem um loop

---
+++ estrutura_controle_reoeticao_quantificada/atividade8.1.rb
@@ -0,0 +1,14 @@
+#crie um script em ruby que lê um número inteiro e mostra a tabuada de multiplicação de numero de 1 a 10
+puts "Dige um numero e saiba sua tabuada"
+numero = gets.chomp.to_i
+
+puts "#{numero} x 1 = #{numero * 1}"
+puts "#{numero} x 2 = #{numero * 2}"
+puts "#{numero} x 3 = #{numero * 3}"
+puts "#{numero} x 4 = #{numero * 4}"
+puts "#{numero} x 5 = #{numero * 5}"
+puts "#{numero} x 6 = #{numero * 6}"
+puts "#{numero} x 7 = #{numero * 7}"
+puts "#{numero} x 8 = #{numero * 8}"
+puts "#{numero} x 9 = #{numero * 9}"
+puts "#{numero} x 10 = #{numero * 10}"
